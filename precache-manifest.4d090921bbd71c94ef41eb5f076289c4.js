41b92089dd32eaf97a06ea7f37760d9a9126b473
Juan Pablo <juanpanime@gmail.com>
2020-02-02 14:10:36 -0500

    Updates

---
+++ precache-manifest.4d090921bbd71c94ef41eb5f076289c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb098f3438d25386ab33bf6b935ee37",
+    "url": "/gastosemanal/index.html"
+  },
+  {
+    "revision": "d3db2698063637603e0d",
+    "url": "/gastosemanal/static/js/2.f57427c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gastosemanal/static/js/2.f57427c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7302d92d5f5b462cc42",
+    "url": "/gastosemanal/static/js/main.db42e770.chunk.js"
+  },
+  {
+    "revision": "712fb0c585a18e8e410c",
+    "url": "/gastosemanal/static/js/runtime-main.2bad344a.js"
+  }
+]);
